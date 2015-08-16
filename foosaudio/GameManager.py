8cec29ada2fa85c298f43a81d2f381da803e037f
Andrew Wang <wang.andrew11@gmail.com>
2015-08-16 14:39:46 -0700

    cleanup function and q checking fixes

--- foosaudio/GameManager.py
+++ foosaudio/GameManager.py
@@ -15,6 +15,8 @@ ARDUINO_SERIAL_ADDR = "/dev/ttyACM0"
 
 class GameManager():
     def __init__(self, sounds_dir):
+        self.arduino = None
+        self.goal_checker = None
         self.goal_q = Queue(1)
         self.sounds = sounds_dir
         self.sound_files = [f for f in listdir(sounds_dir) if isfile(join(sounds_dir, f))]
@@ -36,25 +38,28 @@ class GameManager():
     def check_goal(self, q):
         while True:
             ascii_val = self.arduino.readline()
-            print(ascii_val)
             if "A" in ascii_val:
                 q.put("A")
+                print("Putting A on Q")
             elif "B" in ascii_val:
                 q.put("B")
+                print("Putting B on Q")
             else:
                 pass
 
     def run(self):
-        goal_checker = Process(target=self.check_goal, args=(self.goal_q,))
-        goal_checker.start()
+        self.goal_checker = Process(target=self.check_goal, args=(self.goal_q,))
+        self.goal_checker.start()
 
         while True:
-            if self.goal_q.full() and self.goal_q.get() == "A":
-                self.goal_a()
-            elif self.goal_q.full() and self.goal_q.get() == "B":
-                self.goal_b()
-            else:
-                pass
+            if self.goal_q_full():
+                value = self.goal_q.get()
+                if value == "A":
+                    self.goal_a()
+                elif value == "B":
+                    self.goal_b()
+                else:
+                    pass    # Unknown
 
     def goal_a(self):
         print("Goal A Sound")
@@ -69,6 +74,7 @@ class GameManager():
 
         # Stop the mixer if something's playing
         if pygame.mixer.music.get_busy() == True:
+            print("Stopping whatever's playing")
             pygame.mixer.stop()
 
         pygame.mixer.music.load(file)
@@ -76,8 +82,14 @@ class GameManager():
 
     def cleanup(self):
         try:
-            pygame.mixer.music.quit()
-            self.arduino.close()
+            if self.goal_checker:
+                self.goal_checker.terminate()
+
+            if self.arduino:
+                self.arduino.close()
+
+            pygame.mixer.quit()
+
         except Exception as e:
             print("Cleanup exception: " + str(e))
 
